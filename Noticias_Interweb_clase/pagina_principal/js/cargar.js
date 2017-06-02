6c9585589170d82daf692efd6c6f579fdc01594c
Danichu300 <danikillersx@gmail.com>
2017-06-02 20:44:43 +0200

    cambios

--- Noticias_Interweb_clase/pagina_principal/js/cargar.js
+++ Noticias_Interweb_clase/pagina_principal/js/cargar.js
@@ -12,4 +12,12 @@ $(document).ready(function()
 			$('#botoncargar2').hide();
 		});	
 	});
+	
+		$(window).scroll(function(){
+		if($(window).scrollTop()+$(window).height()>$(document).height()-5){
+			$('#bloque2').show(400);
+			$('#botoncargar').hide();
+			$('#botoncargar2').show();
+		}
+	});
 });
