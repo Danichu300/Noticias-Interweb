9b302f562028e7547a09947c418ddb3dc1a6c4dc
Danichu300 <danikillersx@gmail.com>
2017-06-05 16:55:44 +0200

    Update cargar.js

--- Noticias_Interweb_clase/pagina_principal/js/cargar.js
+++ Noticias_Interweb_clase/pagina_principal/js/cargar.js
@@ -1,14 +1,39 @@
-$(document).ready(function(){
-	$('#botoncargar2').hide();
-	$('#botoncargar').click(function(event) {
-		$('#bloque2').slideDown( "slow", function() {
-			$('#botoncargar').hide();
-			$('#botoncargar2').show();
-		});
-	});
-	$('#botoncargar2').click(function(event) {
-		$('#bloque3').slideDown( "slow", function() {
-			$('#botoncargar2').hide();
-		});	
-	});
+var data1 = null;
+var data2 = null;
+var cargar = 0;
+
+$(function () {
+    $.getJSON("data/pack1.json", function (jsonObject1) {
+        data1 = jsonObject1;
+    });
+    $.getJSON("data/pack2.json", function (jsonObject2) {
+        data2 = jsonObject2;
+    });
+        $(window).scroll(function(){
+        if($(window).scrollTop()+$(window).height()>$(document).height()-1){
+            cargarNews();
+        }
+    });
+    $('#cargarinfo').click(function () {
+        cargarNews();
+    });
 });
+
+function ponerPadre(json) {
+    $.each(json, function (i, pack1) {
+        $('#masnoticias').append($("<div class='col-sm-12'><br><img id='min-pic' src='" + pack1.imagen + "' align='left'><p id='titulo'>" + pack1.title + "</p><br><p id='noticia' >" + pack1.description + "</p></div>"));
+    });
+}
+
+function cargarNews() {
+    if (cargar === 1) {
+        ponerPadre(data2);
+        cargar++;
+        $('#boton').hide();
+    } else {
+        if (cargar === 0) {
+            ponerPadre(data1);
+            cargar++;
+        }
+    }
+}
